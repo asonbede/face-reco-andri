cc6c6b105df5d90f197f96cee2338211eae6fd0f
asonbede <asonbede@gmail.com>
2020-12-30 21:31:35 +0100

    connection string modify

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -18,7 +18,7 @@ const Register = ({ onRouteChange, loadUser }) => {
     console.log("from register1");
     console.log({ nameValue, emailValue, passwordValue });
 
-    fetch("https://face-reco-andri.herokuapp.com/register", {
+    fetch("/register", {
       method: "post",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({
